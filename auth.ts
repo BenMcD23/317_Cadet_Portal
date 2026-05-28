7d060f2a19ff543e7b0b4f9e4c0fad6f645ca77d
BenMcD23 <mcdonaldben02@gmail.com>
2026-05-28 18:52:35 +0100

    fix double auth issues

--- auth.ts
+++ auth.ts
@@ -5,6 +5,14 @@ const ALLOWED_DOMAIN = "317atc.co.uk"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
+  cookies: {
+    sessionToken: { name: "cadet.session-token", options: { httpOnly: true, sameSite: "lax" as const, path: "/", secure: process.env.NODE_ENV === "production" } },
+    callbackUrl: { name: "cadet.callback-url", options: { httpOnly: true, sameSite: "lax" as const, path: "/", secure: process.env.NODE_ENV === "production" } },
+    csrfToken: { name: "cadet.csrf-token", options: { httpOnly: true, sameSite: "lax" as const, path: "/", secure: process.env.NODE_ENV === "production" } },
+    pkceCodeVerifier: { name: "cadet.pkce.code_verifier", options: { httpOnly: true, sameSite: "lax" as const, path: "/", secure: process.env.NODE_ENV === "production" } },
+    state: { name: "cadet.state", options: { httpOnly: true, sameSite: "lax" as const, path: "/", secure: process.env.NODE_ENV === "production" } },
+    nonce: { name: "cadet.nonce", options: { httpOnly: true, sameSite: "lax" as const, path: "/", secure: process.env.NODE_ENV === "production" } },
+  },
   callbacks: {
     ...authConfig.callbacks,
     async jwt({ token, account }) {
